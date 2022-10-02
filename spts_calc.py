e245ddd4a8371ad5036bba041db0acfc78d83572
iamSkev <shawnkenzov@gmail.com>
2022-10-02 19:32:23 +0900

    Added a suggestion for an idk option when asked what the user is training.

--- spts_calc.py
+++ spts_calc.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 STATS = {"fs": 1, "bt": 1.25, "psy": 1.5}
 
-def _converter(stat) -> int | None:
+def _converter(stat) -> int | None:    
     suffix_converter = {
         "k": 1_000,
         "m": 1_000_000,
@@ -34,7 +34,7 @@ def _net(message=None, suffix=True, wrong_type=False) -> int | None:
                 "i'm sorry but you'd have to type that out again since that doesn't look right."
             )
             print(
-                "You can either add a suffix to your stat or type it out manually. e.g 69m or 69000000 "
+                "You can either add a suffix to your stat or type it out manually. e.g., 69m or 69000000 "
             )
             print(
             """ Here are the suffixes:
@@ -56,18 +56,24 @@ def _net(message=None, suffix=True, wrong_type=False) -> int | None:
         else:
             print(
             """ I'm sorry but you'll have to specify which stat you're training between: 
-                fs (Fist Strength)
-                bt (Body Toughness) 
-                psy (Psychic Power)
+                
+            fs (Fist Strength)
+            bt (Body Toughness) 
+            psy (Psychic Power)
+            idk (i don't know)
             """ 
             )
-            stat = str(input("What stat are you training? (fs, bt, psy): ")).lower()
+            stat = str(input("What stat are you training? (fs, bt, psy, idk): ")).lower()
             if STATS.get(stat) is not None:
                 return stat
                 break
 
 def _time_checker(time, msg) -> 0:
     return f"{time} {msg+'s' if time > 1 else msg} " if time != 0 else ""
+
+def _unknown():
+    
+    ...
 
 while True:
     print("You can add suffixes to your stats e.g., 69m will be the same as 69000000")
@@ -82,16 +88,41 @@ while True:
     if per_tick is None:
         per_tick = _net("How much do you get per tick?: ")
 
-    tick_time = str(input("What stat are you training?(fs, bt, psy): ")).lower()
-    if tick_time not in ('fs', 'bt', 'psy'):
+    tick_time = str(input("What stat are you training? (fs, bt, psy, idk): ")).lower()
+    if tick_time not in ('fs', 'bt', 'psy', "idk"):
         tick_time = _net(suffix=False)
-    optimal_time = ((((goal - current) / per_tick) * (STATS.get(tick_time))) / 60) / 60
-    time = datetime.timedelta(
-        hours=optimal_time)
-    days, hours, minutes = time.days, time.seconds // 3600, (time.seconds % 3600) // 60
-    seconds = time.seconds - ((hours*3600)+(minutes*60))
-    milliseconds, microseconds = int((time.microseconds/10000)*10), int((((time.microseconds/10000)*10)-(int((time.microseconds/10000)*10)))*1000)
-    print(
-        f"It would take roughly around {_time_checker(time.days, 'day')}{'and ' if minutes == 0 and hours != 0 else ''}{_time_checker(hours, 'hour')}{'and ' if seconds == 0 and minutes != 0 else ''}{_time_checker(minutes, 'minute')}{'and ' if milliseconds == 0 and seconds != 0 else ''}{_time_checker(seconds, 'second')}{'and ' if microseconds == 0 and milliseconds != 0 else ''}{_time_checker(milliseconds, 'millisecond')}{'and' if microseconds != 0 else ''} {_time_checker(microseconds, 'microsecond')}"
-    )
-    print("Good Luck. And as always. Quack.")
+    elif tick_time == 'idk':
+        total_time = []
+        
+        for k, v in STATS.items():
+            optimal_time = ((((goal - current) / per_tick) * (STATS.get(k))) / 60) / 60
+            time = datetime.timedelta(hours=optimal_time)
+            days, hours, minutes = time.days, time.seconds // 3600, (time.seconds % 3600) // 60
+            seconds = time.seconds - ((hours*3600)+(minutes*60))
+            milliseconds, microseconds = int((time.microseconds/10000)*10), int((((time.microseconds/10000)*10)-(int((time.microseconds/10000)*10)))*1000)
+            total_time.append(f"{_time_checker(time.days, 'day')}{'and ' if minutes == 0 and hours != 0 else ''}{_time_checker(hours, 'hour')}{'and ' if seconds == 0 and minutes != 0 else ''}{_time_checker(minutes, 'minute')}{'and ' if milliseconds == 0 and seconds != 0 else ''}{_time_checker(seconds, 'second')}{'and ' if microseconds == 0 and milliseconds != 0 else ''}{_time_checker(milliseconds, 'millisecond')}{'and' if microseconds != 0 else ''} {_time_checker(microseconds, 'microsecond')}")
+        
+        print(
+        f"""
+        Since you picked idk (i don't know) when i asked you about what you're training, i calculated the time for all 3 of the stats.
+
+        Here they are:
+
+        Fist Strength: {total_time[0]}
+        Body Toughness: {total_time[1]}
+        Psychic Power: {total_time[2]}""")
+        print("""
+        Good Luck. And as always. Quack.
+        """
+        )
+    else:
+        optimal_time = ((((goal - current) / per_tick) * (STATS.get(tick_time))) / 60) / 60
+        time = datetime.timedelta(
+            hours=optimal_time)
+        days, hours, minutes = time.days, time.seconds // 3600, (time.seconds % 3600) // 60
+        seconds = time.seconds - ((hours*3600)+(minutes*60))
+        milliseconds, microseconds = int((time.microseconds/10000)*10), int((((time.microseconds/10000)*10)-(int((time.microseconds/10000)*10)))*1000)
+        print(
+            f"It would take roughly around {_time_checker(time.days, 'day')}{'and ' if minutes == 0 and hours != 0 else ''}{_time_checker(hours, 'hour')}{'and ' if seconds == 0 and minutes != 0 else ''}{_time_checker(minutes, 'minute')}{'and ' if milliseconds == 0 and seconds != 0 else ''}{_time_checker(seconds, 'second')}{'and ' if microseconds == 0 and milliseconds != 0 else ''}{_time_checker(milliseconds, 'millisecond')}{'and' if microseconds != 0 else ''} {_time_checker(microseconds, 'microsecond')}"
+        )
+        print("Good Luck. And as always. Quack.")
